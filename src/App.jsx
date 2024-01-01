fbc4b655508a17fa86e090485007776921f467e4
Abdulmalik <hackobansa@gmail.com>
2024-01-01 18:55:51 +0100

    added files and react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Landing from './components/Landing/Landing'
+
+function App() {
+  return (
+    <>
+      <Landing/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Landing() {
+  return (
+    <main>
+        <nav>
+            <ul>
+                
+            </ul>
+        </nav>
+    </main>
+  )
+}
